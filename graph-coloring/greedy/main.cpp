e3da9b6f96d2a59a335f2977f15d23a84fe27869
Jonathan Provencher <jprovencher@spptel.com>
2021-07-19 19:23:20 -0400

    nouveau makefile tout shiny

--- graph-coloring/greedy/main.cpp
+++ graph-coloring/greedy/main.cpp
@@ -1,9 +1,21 @@
 /*
 *   Author: Joe Provencher -- jprovencher@spptel.com
 *   Last changed: 2021-06-22 16:46:24
-*   Description:  Implémentation de l'algorithme gourmand pour la colorisation de graphes.  Tirés de Diestel - Graph Theory (5ième édition)
+*   Description:  Implémentation de l'algorithme gourmand pour la colorisation de graphes.  Tiré de Diestel - Graph Theory (5ième édition)
 *   License: MIT
 */
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <algorithm>
+#include <list>
+#include "../utils/Vertex.h"
+#include "../utils/Graph.h"
 
 
-
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
