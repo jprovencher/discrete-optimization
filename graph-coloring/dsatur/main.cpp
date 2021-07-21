fb78ba49df3a143ebc600af0e87affaca40e7753
Jonathan Provencher <jprovencher@spptel.com>
2021-07-21 14:05:48 -0400

    Ajout du squelette pour  DSATUR

---
+++ graph-coloring/dsatur/main.cpp
@@ -0,0 +1,17 @@
+/* 
+Implémentation de l'algorithme DSATUR pour colorsation de graphes.
+
+tiré de https://fr.wikipedia.org/wiki/DSATUR
+À haut niveau:
+
+1. Ordonner les sommets par ordre décroissant de degrés.
+2.Colorer un sommet de degré maximum avec la couleur 1.
+3. Choisir un sommet avec DSAT maximum. En cas d'égalité, choisir un sommet de degré maximal.
+4. Colorer ce sommet avec la plus petite couleur possible.
+5. Si tous les sommets sont colorés alors stop. Sinon aller en 3.
+
+Calcul de DSAT
+        DSAT(v)= nombre de couleurs différentes dans les sommets adjacents à v.
+
+ */
+
